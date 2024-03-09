7858bf525bb56dee79b48fc90ffe3f1226dd1da8
Stewart Mackenzie <setori88@gmail.com>
2024-03-09 11:56:07 +0800

    Problem: egui web stuff has totally fallen apart for some reason
    Solution: try implement https://github.com/emilk/eframe_template/issues/116

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './hkcjj_timer.js',
-  './hkcjj_timer_bg.wasm',
+  './hkcjjtimer.js',
+  './hkcjjtimer_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
